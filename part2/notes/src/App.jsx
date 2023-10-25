8bcd1e15ef9325db17ee26885a3efd09b1ff5e1f
Suresh Yadam <suresh.yadam0713@gmail.com>
2023-10-25 19:01:35 +0530

    added notes project

---
+++ part2/notes/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Note from "./components/Note";
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+    console.log("button clicked", event.target);
+  };
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+export default App;
